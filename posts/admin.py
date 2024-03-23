e67f13372c5730691c2783d2d3d1de2ef5e4fdde
Mohamed Gouda <goda61@gmail.com>
2024-03-23 21:11:50 +0200

    Ass category model with relationship

--- posts/admin.py
+++ posts/admin.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 
-from .models import Post
+from .models import Post, Category
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['title','draft']
-    list_filter = ['draft','tags']
+    list_display = ['title','category','draft']
+    list_filter = ['draft','tags','category']
     search_fields = ['title','tags']
 
 
 # Register your models here.
 admin.site.register(Post,PostAdmin)
+admin.site.register(Category)
--- posts/models.py
+++ posts/models.py
@@ -1,12 +1,15 @@
 from django.db import models
 from django.utils import timezone
 from taggit.managers import TaggableManager
+from django.contrib.auth.models import User
+
 
 
 
 # Create your models here.
 
 class Post (models.Model):
+    author = models.ForeignKey(User,related_name='post_author', on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     content = models.TextField(max_length=20000)
     draft = models.BooleanField(default=True)
@@ -14,7 +17,14 @@ class Post (models.Model):
     
     tags = TaggableManager()
     image = models.ImageField(upload_to='post')
+    category = models.ForeignKey('Category', related_name='post_category', on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return self.title
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
     
+    def __str__(self):
+        return self.name
