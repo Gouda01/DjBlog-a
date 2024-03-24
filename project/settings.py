765801ad8f0401fa8c00f78e23175f4a7ee93cb5
Mohamed Gouda <goda61@gmail.com>
2024-03-25 00:02:51 +0200

    Add more and finish lesson 22

--- project/settings.py
+++ project/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     
     # My Packages :
     'taggit',
+    'django_extensions',
 
 
     # My Apps :
@@ -137,3 +138,9 @@ MEDIA_ROOT = BASE_DIR / "media"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+GRAPH_MODELS = {
+  'all_applications': False,
+  'group_models': True,
+}
