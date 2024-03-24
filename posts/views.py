bbdfbb351cc995fa1452982afe700a7483e7903a
Mohamed Gouda <goda61@gmail.com>
2024-03-25 00:40:04 +0200

    Add post list

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
+from .models import Post
+
+
 # Create your views here.
+
+def post_list(request):
+
+    data = Post.objects.all()
+    context = {
+        'object' : data
+    }
+    return render(request,'posts/post_list.html', context)
+
+def post_details():
+    pass
--- project/urls.py
+++ project/urls.py
@@ -19,9 +19,14 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
+from posts.views import post_list,post_details
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    #Posts Url
+    path('posts/', post_list),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
