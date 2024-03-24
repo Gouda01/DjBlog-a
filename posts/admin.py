9e5683bf3558271d6b206fcf3c504c33cd48387f
Mohamed Gouda <goda61@gmail.com>
2024-03-24 22:30:49 +0200

    Add comment model in admin

--- posts/admin.py
+++ posts/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Post, Category
+from .models import Post, Category, Comment
 
 class PostAdmin(admin.ModelAdmin):
     list_display = ['title','category','draft']
@@ -11,3 +11,4 @@ class PostAdmin(admin.ModelAdmin):
 # Register your models here.
 admin.site.register(Post,PostAdmin)
 admin.site.register(Category)
+admin.site.register(Comment)
