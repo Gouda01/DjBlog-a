0f7bf847a266632e91da81e84bcea5def5578bb7
Mohamed Gouda <goda61@gmail.com>
2024-03-29 16:49:27 +0200

    Add pagination in api

--- project/settings.py
+++ project/settings.py
@@ -64,10 +64,12 @@ MIDDLEWARE = [
 ]
 
 
-# To make filter and search in api :
+# To make filter and search and paginare in api :
 
 REST_FRAMEWORK = {
-    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 5
 }
 
 
