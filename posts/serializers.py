9e19306865363b6945a4652e21d4f16802264b22
Mohamed Gouda <goda61@gmail.com>
2024-03-29 16:44:08 +0200

    Show relation in nasted object

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,8 +1,18 @@
 from rest_framework import serializers
+from django.contrib.auth.models import User
 
 from .models import Post
 
+
+class UserSerializers(serializers.ModelSerializer):
+    class Meta :
+        model = User
+        fields = ['id', 'username', 'email']
+
 class PostSerializers(serializers.ModelSerializer):
+    author = UserSerializers()
+    category = serializers.StringRelatedField()
+
     class Meta :
         model = Post
         fields = '__all__'
